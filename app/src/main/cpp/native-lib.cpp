f03a97233024dae135caadbdeaaa0646481e2426
aprildhh <aprildhh@163.com>
2018-12-03 14:54:22 +0800

    指针方式操作像素

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,5 +1,12 @@
 #include <jni.h>
 #include <string>
+#include <android/log.h>
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+using namespace std;
+using namespace cv;
+
 
 extern "C" JNIEXPORT jstring
 
@@ -9,4 +16,82 @@ Java_com_dhh_ndk_1opencv_MainActivity_stringFromJNI(
         jobject /* this */) {
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
+}
+extern "C"
+JNIEXPORT jintArray JNICALL
+Java_com_dhh_ndk_1opencv_MainActivity_grayP(JNIEnv *env, jclass type, jintArray pixels_, jint w,
+                                            jint h) {
+    jint *pixels = env->GetIntArrayElements(pixels_, NULL);
+
+    if (pixels == NULL) {
+        return 0;
+    }
+
+    /**
+     * RGBA  将数组转化成Mat
+     * bitmap中ARGB_8888 实际的通道顺序是BGRA
+     * CV_8UC4 cv_表示在OpenCV中使用，8 代表每一个通道占的位数， U unsigned， C char， 4 通道数
+     */
+    Mat imgData(h, w, CV_8UC4, (unsigned char *) pixels);
+
+//    /**
+//     * 指针方式操作像素
+//     */
+//    uchar *ptr = imgData.ptr(0);
+//
+//    //获取当前的CPU钟摆时间
+//    double time = static_cast<double >(getTickCount());
+//
+//    //中间是运行过程
+//    for (int i = 0; i < w * h; ++i) {
+//        /**
+//         * 灰度值计算公式  像素灰度值 = R*0.3+ G*0.59 + B*0.11
+//         * 从RGB转Y（亮度）UV得出来的
+//         * 矩阵的第一行就是一个灰度值
+//         */
+//        uchar gray = (uchar) (ptr[4 * i + 2] * 0.299 + ptr[4 * i + 1] * 0.587 +
+//                              ptr[4 * i + 0] * 0.114);
+//        ptr[4 * i + 0] = gray;
+//        ptr[4 * i + 1] = gray;
+//        ptr[4 * i + 2] = gray;
+//
+//    }
+//
+//    //计算运行时间
+//    time = ((double) getTickCount() - time) / getTickFrequency();
+//    __android_log_print(ANDROID_LOG_ERROR,"JNI","%ld",time);
+
+
+  //获取当前的CPU钟摆时间
+    double time = static_cast<double >(getTickCount());
+
+    //中间是运行过程
+    for (int i = 0; i < w * h; ++i) {
+        /**
+         * 灰度值计算公式  像素灰度值 = R*0.3+ G*0.59 + B*0.11
+         * 从RGB转Y（亮度）UV得出来的
+         * 矩阵的第一行就是一个灰度值
+         */
+        uchar gray = (uchar) (ptr[4 * i + 2] * 0.299 + ptr[4 * i + 1] * 0.587 +
+                              ptr[4 * i + 0] * 0.114);
+        ptr[4 * i + 0] = gray;
+        ptr[4 * i + 1] = gray;
+        ptr[4 * i + 2] = gray;
+
+    }
+
+    //计算运行时间
+    time = ((double) getTickCount() - time) / getTickFrequency();
+    __android_log_print(ANDROID_LOG_ERROR,"JNI","%ld",time);
+
+    //腐蚀效果
+//    Mat ppt = getStructuringElement(MORPH_RECT,Size(50,50));
+//    erode(imgData,imgData,ppt);
+
+    int size = w * h;
+    jintArray result = env->NewIntArray(size);
+    env->SetIntArrayRegion(result, 0, size, pixels);
+
+    env->ReleaseIntArrayElements(pixels_, pixels, 0);
+    return result;
 }
--- app/src/main/java/com/dhh/ndk_opencv/MainActivity.java
+++ app/src/main/java/com/dhh/ndk_opencv/MainActivity.java
@@ -1,10 +1,18 @@
 package com.dhh.ndk_opencv;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView sampleText;
+    private Bitmap bitmap;
+
 
     // Used to load the 'native-lib' library on application startup.
     static {
@@ -17,8 +25,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView ( R.layout.activity_main );
 
         // Example of a call to a native method
-        TextView tv = (TextView) findViewById ( R.id.sample_text );
-        tv.setText ( stringFromJNI () );
+        sampleText = (ImageView) findViewById ( R.id.sample_text );
+        bitmap = BitmapFactory.decodeResource ( getResources (), R.mipmap.timg );
+
+        stringFromJNI ();
     }
 
     /**
@@ -26,4 +36,17 @@ public class MainActivity extends AppCompatActivity {
      * which is packaged with this application.
      */
     public native String stringFromJNI();
+
+    public static native int[] grayP(int[] pixels, int w, int h);
+
+    public void changeColor(View view) {
+        int w = bitmap.getWidth ();
+        int h = bitmap.getHeight ();
+        int[] pixels = new int[w * h];
+        bitmap.getPixels ( pixels, 0, w, 0, 0, w, h );
+        int[] resultInt = grayP ( pixels, w, h );
+        Bitmap resultImg = Bitmap.createBitmap ( w,h,Bitmap.Config.ARGB_8888 );
+        resultImg.setPixels ( resultInt,0,w,0,0,w,h );
+        sampleText.setImageBitmap ( resultImg );
+    }
 }
